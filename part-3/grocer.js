d861e3bb6b7ab2f6ec5850a2794d2309474abf43
Aaron Boatwright <boatwrightaaron@gmail.com>
2017-06-21 03:09:06 -0700

    adds proto shell..

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,44 +1,52 @@
-// Global vars
+// // Global vars
+//
+// let total = 0
+//     itemCount = []
+//     shoppingCart = []
+//
+//
+// // Add item to shopping cart
+// let addToCart = () => {
+//
+// }
+// // Removes all items from shopping cart
+// let clearCart = () => {
+//     itemCount = 0
+//     document.getElementByID('cart-item-count').innerHTML = '(${itemCount})'
+//
+// }
+// // Close shopping cart window
+// let clossModal = () => {
+//
+// }
+// // Show items in cart
+// let showCart = () => {
+//
+// }
+//
+// // Modal
+// let modal = document.getElementById('myModal')
+//     modalButton = document.getElementById('cart-button')
+//     span = document.getElementById('close')[0]
+//
+// modalButton.onclick = () => {
+//   modal.style.display = 'block'
+// }
+//
+// span.onclick = () => {
+//   modal.style.display = 'none'
+// }
+//
+// window.onlick = () => {
+//   if(event.target == modal) {
+//     modal.style.display = ''
+//   }
+// }
+//
+// // Maybe true prototype?
 
-let total = 0
-    itemCount = []
-    shoppingCart = []
-
-
-// Add item to shopping cart
-let addToCart = () => {
-
-}
-// Removes all items from shopping cart
-let clearCart = () => {
-    itemCount = 0
-    document.getElementByID('cart-item-count').innerHTML = '(${itemCount})'
-
-}
-// Close shopping cart window
-let clossModal = () => {
-
-}
-// Show items in cart
-let showCart = () => {
-
-}
-
-// Modal
-let modal = document.getElementById('myModal')
-    modalButton = document.getElementById('cart-button')
-    span = document.getElementById('close')[0]
-
-modalButton.onclick = () => {
-  modal.style.display = 'block'
-}
-
-span.onclick = () => {
-  modal.style.display = 'none'
-}
-
-window.onlick = () => {
-  if(event.target == modal) {
-    modal.style.display = ''
-  }
+function Cart(items) {
+  this.cartCount = 0
+  this.shoppingList = document.querySelector('#cart-button')
+  this.total = 0
 }
