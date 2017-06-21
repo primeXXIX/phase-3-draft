cedb1b77ad055a4837d08ee9c2f53252a2453c1b
Aaron Boatwright <boatwrightaaron@gmail.com>
2017-06-21 10:36:29 -0700

    adds html modal view

--- part-3/grocer.js
+++ part-3/grocer.js
@@ -1,52 +1,46 @@
-// // Global vars
-//
-// let total = 0
-//     itemCount = []
-//     shoppingCart = []
-//
-//
-// // Add item to shopping cart
-// let addToCart = () => {
-//
-// }
-// // Removes all items from shopping cart
-// let clearCart = () => {
-//     itemCount = 0
-//     document.getElementByID('cart-item-count').innerHTML = '(${itemCount})'
-//
-// }
-// // Close shopping cart window
-// let clossModal = () => {
-//
-// }
-// // Show items in cart
-// let showCart = () => {
-//
-// }
-//
-// // Modal
-// let modal = document.getElementById('myModal')
-//     modalButton = document.getElementById('cart-button')
-//     span = document.getElementById('close')[0]
-//
-// modalButton.onclick = () => {
-//   modal.style.display = 'block'
-// }
-//
-// span.onclick = () => {
-//   modal.style.display = 'none'
-// }
-//
-// window.onlick = () => {
-//   if(event.target == modal) {
-//     modal.style.display = ''
-//   }
-// }
-//
-// // Maybe true prototype?
+// Global vars
 
-function Cart(items) {
-  this.cartCount = 0
-  this.shoppingList = document.querySelector('#cart-button')
-  this.total = 0
+let total = 0
+    itemCount = []
+    shoppingCart = []
+
+
+// Add item to shopping cart
+let addToCart = () => {
+
 }
+// Removes all items from shopping cart
+let clearCart = () => {
+    itemCount = 0
+    document.getElementByID('cart-item-count').innerHTML = '(${itemCount})'
+
+}
+// Close shopping cart window
+let clossModal = () => {
+
+}
+// Show items in cart
+let showCart = () => {
+
+}
+
+// Modal
+let modal = document.getElementById('myModal')
+    modalButton = document.getElementById('cart-button')
+    span = document.getElementById('close')[0]
+
+modalButton.onclick = () => {
+  modal.style.display = 'block'
+}
+
+span.onclick = () => {
+  modal.style.display = 'none'
+}
+
+window.onlick = () => {
+  if(event.target == modal) {
+    modal.style.display = ''
+  }
+}
+
+// Maybe prototype?
